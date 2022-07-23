08146e2149b892edec0fe9404f6b89ddf546a291
YonitTzairi <yonit@xyzeron.com>
2022-07-23 17:18:15 +0300

    added units change

--- src/constants.js
+++ src/constants.js
@@ -1,9 +1,9 @@
 export const constants= {
 	apiKey:'?apikey=a6G5iYMGG7hF7iiEnGgh41oa4pANsd46',
 	telAviv:'215854',
-	oneCityUrl:'http://dataservice.accuweather.com/locations/v1/',
-	locationsUrl:`http://dataservice.accuweather.com/locations/v1/cities/autocomplete`,
-	getCurrentWeatherUrl:'http://dataservice.accuweather.com/currentconditions/v1/',
-	getFiveDayLocationUrl:'http://dataservice.accuweather.com/forecasts/v1/daily/5day/',
-	lanLangUrl:'http://dataservice.accuweather.com/locations/v1/cities/geoposition/search'
+	oneCityUrl:'https://dataservice.accuweather.com/locations/v1/',
+	locationsUrl:`https://dataservice.accuweather.com/locations/v1/cities/autocomplete`,
+	getCurrentWeatherUrl:'https://dataservice.accuweather.com/currentconditions/v1/',
+	getFiveDayLocationUrl:'https://dataservice.accuweather.com/forecasts/v1/daily/5day/',
+	lanLangUrl:'https://dataservice.accuweather.com/locations/v1/cities/geoposition/search'
 }
